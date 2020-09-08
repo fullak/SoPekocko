19cc9c660e1f924952b4935587949daa76b62861
fullak <brochardtristan@gmail.com>
2020-09-08 12:57:28 +0200

    add models sauces.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 require('dotenv').config()
 
 const userRoutes = require('./routes/user');
@@ -29,7 +30,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/auth', userRoutes);
 
