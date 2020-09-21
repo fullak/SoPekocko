57f2341b0cc4d06f31f4d1c89ae12df4af041b2c
Tristan Brochard <brochardtristan@gmail.com>
2020-09-21 10:32:03 +0200

    add express-rate-limit

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const sauceRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/users');
 const path = require('path');
 const helmet = require('helmet'); //protéger l'application de certaines des vulnérabilités bien connues du Web
+const rateLimit = require("express-rate-limit"); // rate limiter prevenir le bruteforce de l'authentification
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}${process.env.DB_HOST}/${process.env.DB_COLL}?retryWrites=true&w=majority`,
 	{ useNewUrlParser: true,
@@ -26,9 +27,16 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD
     );
     next();
   }); 
+
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  });
   
 app.use(bodyParser.json());
 app.use(helmet());
+app.use(limiter);
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -4,9 +4,9 @@ const saucesCtrl = require('../controllers/sauces');
 const multer = require('../middleware/multer-config')
 const auth = require('../middleware/auth');
 
-router.get('/', auth, multer, saucesCtrl.getAllSauces);
+router.get('/', auth, saucesCtrl.getAllSauces);
 router.post('/', auth, multer, saucesCtrl.createSauce);
-router.get('/:id', auth, multer, saucesCtrl.getOneSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
 router.delete('/:id', auth, multer, saucesCtrl.deleteSauce);
 router.post('/:id/like', auth, saucesCtrl.like);
