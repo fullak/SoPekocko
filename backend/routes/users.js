facfda1e6bddb5d8ee624a47c00b397951836933
Tristan Brochard <brochardtristan@gmail.com>
2020-09-21 10:10:18 +0200

    add verifyPasswordStrength in middleware

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/users');
+const verifyPasswordStrength = require('../middleware/verifyPasswordStrength')
 
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', verifyPasswordStrength, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
